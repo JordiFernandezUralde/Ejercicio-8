334855c7639eec35f13d8600d17d4d5cbd84c990
Jon <jon.garay@ikasle.egibide.org>
2024-04-16 13:06:25 +0200

    Crear Clase Personas

--- Ejercicio 8 Git/src/Modelo/Personas.java
+++ Ejercicio 8 Git/src/Modelo/Personas.java
@@ -1,4 +1,30 @@
 package Modelo;
 
 public class Personas {
+    private String dni;
+    private String nombre;
+
+    public Personas(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public Personas() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
