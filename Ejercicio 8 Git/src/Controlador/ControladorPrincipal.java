7d17a328a11cc7ff22671b667c4e9f4f4d0c0e90
jordi <jordi.fernandez@ikasle.egibide.org>
2024-04-16 13:17:40 +0200

    ControladorPrincipal

--- Ejercicio 8 Git/src/Controlador/ControladorPrincipal.java
+++ Ejercicio 8 Git/src/Controlador/ControladorPrincipal.java
@@ -1,4 +1,12 @@
 package Controlador;
 
+import Vista.VentanaPrincipal;
+
 public class ControladorPrincipal {
+    private VentanaPrincipal vp;
+
+    public ControladorPrincipal() {
+        vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }
 }
