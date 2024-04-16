87ea6c06ed217d567bc9a896d92a4fee18cae33f
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-04-16 13:24:28 +0200

    Ventana Principal Sin Botones

--- Ejercicio 8 Git/src/Vista/VentanaPrincipal.java
+++ Ejercicio 8 Git/src/Vista/VentanaPrincipal.java
@@ -2,6 +2,14 @@ package Vista;
 
 import javax.swing.*;
 
-public class VentanaPrincipal {
-    private JPanel panel1;
+public class VentanaPrincipal extends JFrame{
+    private JPanel pPrincipal;
+    private JButton salirButton;
+
+    public VentanaPrincipal(){
+        this.setContentPane(pPrincipal);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
